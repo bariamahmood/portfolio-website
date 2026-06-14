9732f73f44c7ca981c11d7ce82a201cd7d58a3bd
bariamahmood <baria.mahmood26@gmail.com>
2026-06-14 20:24:15 +0000

    Improve dark mode button text

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const themeBtn =document.getElementById("theme-btn");
 
 themeBtn.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.textContent = "Light Mode";
+    } else {
+        themeBtn.textContent = "Dark Mode";
+    }
+
 });
 
 const contactBtn = document.getElementById("contact-btn");
