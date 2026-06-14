7a10a4c722bc99489416555f6aaf3674b67a7cb7
bariamahmood <baria.mahmood26@gmail.com>
2026-06-14 20:18:23 +0000

    Implement dark mode functionality

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log("Portfolio website loaded successfully!");
 
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
+
+const themeBtn =document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
